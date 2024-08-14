eb525efe11251ebbd9965f30a8ed4e02be33cb51
mieras <mieras@gmail.com>
2024-08-14 09:50:12 +0200

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
-import icon from "astro-icon"
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
